3d26eef9eebfecd7165855856876238e7e8b470a
lh <1050417395@qq.com>
2022-11-18 15:59:57 +0800

    init push

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1. 校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 如果不合理// 2. 如果不符合 错
+            return Result.fail("手机格式错误！");
+        }
+        // 3. 如果符合
+
+        // 3.1 生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 3.2 保存验证码
+        session.setAttribute("code", code);
+        // 3.3 发送
+        log.debug("发送短信验证码成功，验证码：{}", code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. 发送loginForm
+        
+
+
+
+        return Result.ok();
+    }
+}
